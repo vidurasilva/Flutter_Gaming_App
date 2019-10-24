6af56a2d21c0ca9b70b94303b32860385158a15d
vidurasilva <kvidurachathuranga@gmail.com>
2019-10-24 23:39:37 +0530

    Add Pokey Layout

---
+++ lib/Screen/pokedex/pokedex.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:game_app/screen/pokedex/widgets/generation_modal.dart';
+import 'package:game_app/screen/pokedex/widgets/search_modal.dart';
+import 'package:game_app/widgets/fab.dart';
+import 'package:game_app/widgets/poke_container.dart';
+import 'package:game_app/widgets/pokemon_card.dart';
+
+
+
+class Pokedex extends StatefulWidget {
+  @override
+  _PokedexState createState() => _PokedexState();
+}
+
+class _PokedexState extends State<Pokedex> with SingleTickerProviderStateMixin {
+  Animation<double> _animation;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation = CurvedAnimation(curve: Curves.easeInOut, parent: _controller);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
+  }
+
+  void _showSearchModal() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => SearchBottomModal(),
+    );
+  }
+
+  void _showGenerationModal() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) => GenerationModal(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          PokeContainer(
+            appBar: true,
+            children: <Widget>[
+              SizedBox(height: 34),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                child: Text(
+                  "Pokedex",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+              Expanded(
+                child: GridView.builder(
+                  physics: BouncingScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.4,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  padding: EdgeInsets.only(left: 28, right: 28, bottom: 58),
+                  itemCount: pokemons.length,
+                  itemBuilder: (context, index) => PokemonCard(
+                    pokemons[index],
+                    index: index,
+                    onPress: () {
+                      Navigator.of(context).pushNamed("/pokemon-info");
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+          AnimatedBuilder(
+            animation: _animation,
+            builder: (_, __) {
+              return IgnorePointer(
+                ignoring: _animation.value == 0,
+                child: InkWell(
+                  onTap: () {
+                    _controller.reverse();
+                  },
+                  child: Container(
+                    color: Colors.black.withOpacity(_animation.value * 0.5),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: ExpandedAnimationFab(
+        items: [
+          FabItem(
+            "Favourite Pokemon",
+            Icons.favorite,
+            onPress: () {
+              _controller.reverse();
+            },
+          ),
+          FabItem(
+            "All Type",
+            Icons.filter_vintage,
+            onPress: () {
+              _controller.reverse();
+            },
+          ),
+          FabItem(
+            "All Gen",
+            Icons.flash_on,
+            onPress: () {
+              _controller.reverse();
+              _showGenerationModal();
+            },
+          ),
+          FabItem(
+            "Search",
+            Icons.search,
+            onPress: () {
+              _controller.reverse();
+              _showSearchModal();
+            },
+          ),
+        ],
+        animation: _animation,
+        onPress: () {
+          if (_controller.isCompleted) {
+            _controller.reverse();
+          } else {
+            _controller.forward();
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:game_app/Screen/homePage.dart';
+import 'package:game_app/Screen/pokedex/pokedex.dart';
+import 'package:game_app/configs/AppColors.dart';
+import 'package:game_app/screen/pokemon_info/pokemon_info.dart';
+import 'package:game_app/widgets/fade_page_route.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,11 +13,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.white,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        fontFamily: 'CircularStd',
+        textTheme: Theme.of(context).textTheme.apply(displayColor: AppColors.black),
+        scaffoldBackgroundColor: AppColors.lightGrey,
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Gaming App Home Page'),
+      onGenerateRoute: _getRoute,
     );
+  }
+
+  Route _getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return FadeRoute(page: MyHomePage());
+
+      case '/pokedex':
+        return FadeRoute(page: Pokedex());
+
+      case '/pokemon-info':
+        return FadeRoute(page: PokemonInfo());
+
+      default:
+        return null;
+    }
   }
 }
