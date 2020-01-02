3b29f4a38ce5be5fca69845f3e382b587e72d0aa
vidurasilva <kvidurachathuranga@gmail.com>
2020-01-02 21:43:07 +0530

    Vategory List Update

--- lib/Screen/home/widgets/category_list.dart
+++ lib/Screen/home/widgets/category_list.dart
@@ -4,17 +4,18 @@ import 'package:game_app/widgets/poke_category_card.dart';
 class CategoryList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 2.44,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 12,
-      ),
-      padding: EdgeInsets.only(left: 28, right: 28, bottom: 58),
-      itemCount: categories.length,
+    List categories = new List();
+        return GridView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2.44,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 12,
+          ),
+          padding: EdgeInsets.only(left: 28, right: 28, bottom: 58),
+          itemCount: categories.length,
       itemBuilder: (context, index) => PokeCategoryCard(
         categories[index],
         onPress: () => Navigator.of(context).pushNamed("/pokedex"),
