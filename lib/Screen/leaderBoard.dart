af0f967963db4b43b86ea75c7ccfd3e0254ad91e
vidurasilva <kvidurachathuranga@gmail.com>
2020-01-11 22:43:22 +0530

    leader boadr update

--- lib/Screen/leaderBoard.dart
+++ lib/Screen/leaderBoard.dart
@@ -9,7 +9,21 @@ class _LeaderboardState extends State<Leaderboard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Text('data'),
+          ),
+          Container(
+            child: ListView(
+              children: <Widget>[
+                Text('Vidura'),
+                Text('chthursngs'),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
